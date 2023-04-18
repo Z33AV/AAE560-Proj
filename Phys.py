35db1c9d85db5bc341cd64ee901263870e2cccd1
Sarah Reilly <sarahlr1999@gmail.com>
2023-04-18 13:24:25 -0400

    Testing

--- Phys.py
+++ Phys.py
@@ -1,5 +1,11 @@
 #physics file and methods definitions
 import math
+import numpy
 import mesa
 
-yeet = 5.0
+#
+# Node parent node class w/ inheritance
+# Attributes:
+# current location (x,y: float) 
+# orbits (a,ex,ey,i,raan,f: float)
+# resource level (normalized float)
