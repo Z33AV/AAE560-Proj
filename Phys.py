9e0c085d550c2e10930e09797260419353cb46a1
Sarah Reilly <sarahlr1999@gmail.com>
2023-04-22 17:18:39 -0400

    Physics file definition set up as of end-of-meeting, 4/22

--- Phys.py
+++ Phys.py
@@ -11,3 +11,31 @@ import mesa
 
 =======
 #yeet = 5.0
+
+# # Methods called on node objects 
+# def PlaceNode(node):
+#     # Method to place the fixed nodes at initial time
+#     # Inputs: self
+#     # physically places nodes on grid (build our own grid)
+#     return
+    
+
+# def PlaceTransporters(transporter):
+#     # Method to place the transporters at initial time
+#     # Inputs:
+#     # 
+#     return
+
+def StepOrbit(agent,dt):
+    # Inputs: dt (float)
+    # Method to step object to dt along its orbi
+    #
+    return
+
+def ComputeTransfer(transporter):
+    # Method to compute the orbit transfer between nodes
+    # Inputs: 
+    # Outputs: dictionary containing {"isPossible": bool, "dV": float, "cost": float}
+    # 
+    return
+
